f0c682bf4d64e2ccaffefb86b1098914fb0f525c
Platos-Man <joonasru@tutanota.com>
2023-05-11 13:27:41 +0300

    entity now moves randomly and screen ends loop

--- core/main.py
+++ core/main.py
@@ -1,5 +1,5 @@
 import sys
-from random import randint
+from random import choice, randint
 
 import pygame
 
@@ -11,7 +11,7 @@ class MainRun:
         self.dw = dw  # Display width
         self.dh = dh  # Display height
         self.window = pygame.display.set_mode((self.dw, self.dh))
-        self.grid_size = 20
+        self.grid_size = 100
         self.main()
 
     def main(self):
@@ -22,12 +22,14 @@ class MainRun:
             self.draw_grid()
 
             entity.draw_entity(self.window)
+            entity.move_entity()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()
-            window_clock.tick(60)
+            window_clock.tick(2)
 
     def draw_grid(self):
         for x in range(0, self.dw, self.grid_size):
@@ -40,8 +42,10 @@ class Entity:
     def __init__(self, dw, dh, grid_size):
         self.color = (200, 0, 0)
         self.grid_size = grid_size
-        self.xpos = randint(0, (dw - self.grid_size) // self.grid_size)
-        self.ypos = randint(0, (dh - self.grid_size) // self.grid_size)
+        self.screen_xend = (dw - self.grid_size) // self.grid_size
+        self.screen_yend = (dh - self.grid_size) // self.grid_size
+        self.xpos = randint(0, self.screen_xend)
+        self.ypos = randint(0, self.screen_yend)
 
     def draw_entity(self, window):
         body = pygame.Rect(
@@ -51,6 +55,21 @@ class Entity:
             self.grid_size,
         )
         pygame.draw.rect(window, self.color, body)
+
+    def move_entity(self):
+        state = choice([0, 1])
+        if state:
+            self.xpos += choice([-1, 0, 1])
+            if self.xpos < 0:
+                self.xpos = self.screen_xend
+            if self.xpos > self.screen_xend:
+                self.xpos = 0
+        else:
+            self.ypos += choice([-1, 0, 1])
+            if self.ypos < 0:
+                self.ypos = self.screen_yend
+            if self.ypos > self.screen_yend:
+                self.ypos = 0
 
 
 if __name__ == "__main__":
