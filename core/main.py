0ed9275c4b1de8ec3e71c2f4d78df5b13c6db1f5
Platos-Man <joonasru@tutanota.com>
2023-05-24 09:53:34 +0300

    added graphics for snake

--- core/main.py
+++ core/main.py
@@ -1,21 +1,44 @@
 import sys
 from random import choice, randint
+from pygame.math import Vector2
 
 import pygame
 
 
 class Snake:
     def __init__(self):
-        pass
+        self.body = [Vector2(20, 15), Vector2(20, 16), Vector2(20, 17), Vector2(19, 17)]
+        self.directon = Vector2(0, 0)
+
+        self.head_up = pygame.image.load("sprites/snake_head.png")
+
+        self.tail_up = pygame.image.load("sprites/snake_tail.png")
+        self.tail_down = pygame.transform.rotate(self.tail_up, 180)
+        self.tail_left = pygame.transform.rotate(self.tail_up, 90)
+        self.tail_right = pygame.transform.rotate(self.tail_up, -90)
+
+        self.body_vertical = pygame.image.load("sprites/snake_body.png")
+
+        self.body_tl = pygame.image.load("sprites/snake_body_turn.png")
+
+    def draw_snake(self):
+        for idx, block in enumerate(self.body):
+            block_rect = pygame.Rect(block.x * grid_size, block.y * grid_size, grid_size, grid_size)
+            if idx == 0:
+                screen.blit(self.head_up, block_rect)
+            # placeholder for logic
+            elif idx == 1:
+                screen.blit(self.body_vertical, block_rect)
+            elif idx == 2:
+                screen.blit(self.body_tl, block_rect)
+            elif idx == 3:
+                screen.blit(self.tail_right, block_rect)
 
 
 class Main:
     def __init__(self):
-        self.grid_size = 20
-        self.cells_in_row = 40
-        self.cells_in_col = 30
-        self.screen_w = self.grid_size * self.cells_in_row
-        self.screen_h = self.grid_size * self.cells_in_col
+        self.screen_w = grid_size * cells_in_row
+        self.screen_h = grid_size * cells_in_col
 
         self.snake = Snake()
 
@@ -34,19 +57,24 @@ class Main:
             clock.tick(60)
 
     def draw_grid(self):
-        for x in range(0, self.screen_w, self.grid_size):
-            for y in range(0, self.screen_h, self.grid_size):
-                grid_block = pygame.Rect(x, y, self.grid_size, self.grid_size)
+        for x in range(0, self.screen_w, grid_size):
+            for y in range(0, self.screen_h, grid_size):
+                grid_block = pygame.Rect(x, y, grid_size, grid_size)
                 pygame.draw.rect(screen, (150, 150, 150), grid_block, 1)
 
     def draw_elements(self):
         self.draw_grid()
+        self.snake.draw_snake()
 
     def update(self):
         pass
 
 
 pygame.init()
+
+grid_size = 20
+cells_in_row = 40  # X
+cells_in_col = 30  # Y
 
 main = Main()
 screen = pygame.display.set_mode((main.screen_w, main.screen_h))
